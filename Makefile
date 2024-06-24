4034b3b4f4642d24285ced9e2b3a8418afea2356
Tyler Smith <tylertms@icloud.com>
2024-06-24 16:39:30 -0400

    Initial cleanup

---
+++ Makefile
@@ -0,0 +1,16 @@
+CFLAGS = -O2 -lz -I"./src/lib" -Wall -pedantic -fsanitize=address -g
+
+ifeq ($(OS),Windows_NT)
+	LDFLAGS = -lwinhttp
+else
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Darwin)
+		LDFLAGS = -framework Foundation
+		CFLAGS += -Wno-gnu-zero-variadic-macro-arguments
+	else ifeq ($(UNAME_S),Linux)
+		LDFLAGS = -lcurl -lpthread
+	endif
+endif
+
+rpotool: src/*.c src/lib/*.c
+	gcc $^ -o $@ $(CFLAGS) $(LDFLAGS)
--- config/config.py
+++ config/config.py
@@ -23,22 +23,23 @@ def get_config() -> ei_pb.ConfigResponse:
 
 def parse_config():
     config = get_config()
+    print(config)
     dlc_catalog = config.dlc_catalog
 
-    with open("shells.csv", "w") as f:
+    with open("shells.txt", "w") as f:
         sets = {}
         asset_types = []
         chicken_types = []
 
         for shell_set in dlc_catalog.shell_sets:
-            shell_set.name = shell_set.name.replace(",", "")
+            shell_set.name = shell_set.name
             sets[shell_set.identifier] = shell_set.name
-            f.write(f"set,{shell_set.identifier},{shell_set.name}\n")
+            # f.write(f"set,{shell_set.identifier},{shell_set.name}\n")
 
         for decorator in dlc_catalog.decorators:
-            decorator.name = decorator.name.replace(",", "")
+            decorator.name = decorator.name
             sets[decorator.identifier] = decorator.name
-            f.write(f"decorator,{decorator.identifier},{decorator.name}\n")
+            # f.write(f"decorator,{decorator.identifier},{decorator.name}\n")
     
         for shell in dlc_catalog.shells:
             set_name = "NO SET"
@@ -48,12 +49,13 @@ def parse_config():
                 set_name = shell.name
 
             type = ei_pb.ShellSpec.AssetType.Name(shell.primary_piece.asset_type)
-            url_key = shell.primary_piece.dlc.url.split("/")[-1]
+            url_key = shell.primary_piece.dlc.url.split("_")[-1].split(".")[0]
+            original_size = shell.primary_piece.dlc.original_size
 
             if type not in asset_types:
                 asset_types.append(type)
 
-            f.write(f"shell,{shell.identifier},{set_name},{type},{url_key}\n")
+            f.write(f"shell|{set_name} - {type}|{shell.identifier}|{url_key}|{original_size}\n")
 
         for shell_object in dlc_catalog.shell_objects:
 
@@ -64,14 +66,8 @@ def parse_config():
                 chicken_types.append(object_name)
 
             for piece in shell_object.pieces:
-                url_key = piece.dlc.url.split("/")[-1]
-                f.write(f"chicken,{piece.dlc.name},{object_name},{type},{url_key}\n")
-
-        for asset_type in asset_types:
-            f.write(f"shell_type,{asset_type}\n")
-        
-        for chicken_type in chicken_types:
-            f.write(f"chicken_type,{chicken_type}\n")
+                url_key = piece.dlc.url.split("_")[-1].split(".")[0]
+                f.write(f"chicken|{object_name}|{piece.dlc.name}|{url_key}|{piece.dlc.original_size}\n")
         
 
 if __name__ == "__main__":
